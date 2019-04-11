6fbadf40433776c89b1d5c1bba9cc2d5374a845c
Dipen Chauhan <get.dipen@gmail.com>
2019-04-10 18:28:58 -0700

    Added detect

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@
 #                      root GET  /                                                                                        welcome#index
 #                     index GET  /index(.:format)                                                                         welcome#index
 #                      show GET  /show(.:format)                                                                          welcome#show
+#                    detect GET  /detect(.:format)                                                                        welcome#detect
 #        rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
@@ -14,5 +15,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'index', to: 'welcome#index'
   get 'show', to: 'welcome#show'
+  get 'detect', to: 'welcome#detect'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
